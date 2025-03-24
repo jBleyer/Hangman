4eaefc483c9911de927fba8a987865771ab391df
jBleyer <162551911+jBleyer@users.noreply.github.com>
2025-03-24 09:16:40 +0100

    second commit

--- src/hangman.java
+++ src/hangman.java
@@ -1,5 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class hangman {
     private JPanel mainPanel;
+    private JTextField eingabeTextField;
+    private JButton suchenBtn;
+    private JLabel wortLabel;
+    private JButton neustartBtn;
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("hanfman gui");
+        frame.setContentPane(new hangman().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(500, 300);
+        frame.setForeground(new Color(238, 130, 238));
+    }
 }
